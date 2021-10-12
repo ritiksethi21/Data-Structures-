8ec16190591136c5f485efc9e9916b90d6adc4d8
Ritik Sethi <58940080+ritiksethi21@users.noreply.github.com>
2021-10-12 10:35:20 +0530

    Create Is_Binary_Number_Multiple_Of_3.cpp

---
+++ Is_Binary_Number_Multiple_Of_3.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+	
+	int isDivisible(string s){
+	    int sum = 0, x = 1;
+	    for(int i = s.length() - 1; i >= 0; i--){
+	        if((s[i] - '0') == 1){
+	            sum = (sum + x) % 3;
+	        }
+	        x = (x * 2) % 3;
+	    }
+
+	    if(sum == 0) return 1;
+	    return 0;
+	}
+};
+
+int main(){
+    
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.isDivisible(s) << endl;
+    }
+return 0;
+}
